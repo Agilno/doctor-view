581698e4e35709be2bef5b7e05754f114525798c
Petar <petar@petar.local>
2020-12-17 12:29:05 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        cyan: colors.cyan,
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ]
+}
